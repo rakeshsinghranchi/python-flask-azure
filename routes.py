69fcd953713c589a65b518c97a2e5bfcd2d48993
Rakesh Singh <raksingh@microsoft.com>
2018-03-11 04:02:19 -0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,11 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+if __name__ == '__main__':
+  app.run()
