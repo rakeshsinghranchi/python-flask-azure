ed0cebb195dca541dd17be28e4abe0147e7eba03
Rakesh Singh <raksingh@microsoft.com>
2018-03-15 19:27:20 -0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World from Rakesh!"
+    return "Hello, World from Rakesh updated!"
 
 if __name__ == '__main__':
   app.run()
